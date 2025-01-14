0e3c8e098f12a4b50ccd4fc08b03ccc983fb51b7
Your Name <you@example.com>
2025-01-14 15:58:43 -0800

    Add hashTable_expand

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,8 +1,8 @@
-//*****************************************************************************
+//*****************************************************************************************
 
 //  hash_table.h  -   Hash Table header file
 
-//*****************************************************************************
+//*****************************************************************************************
 
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
@@ -10,21 +10,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define INITIAL_CAPACITY    10
-#define MAX_WORD            256
+#define INITIAL_CAPACITY        10
+#define MAX_WORD                256
 
-//*****************************************************************************
+//*****************************************************************************************
 
-typedef struct {                    // Word structure
+typedef struct {                    // Word structure - hash table entry
     char data[MAX_WORD];
     int freq;
 } word;
 
-word *hash_table[TABLE_SIZE];       // Declare the hash Table
+typedef struct {                    // hashTable structure 
+    word* entries;                  // a pointer to the first element 
+    size_t capacity;                // total number of slots in the hash table
+    size_t length;                  // total number of actual items in the hash table
+} hashTable;  
 
-int *table_capacity;                // Capacity of the hash Table
-
-//*****************************************************************************
+//*****************************************************************************************
 
 // Function declaration for hash table 
 unsigned long hash(unsigned char *str);
@@ -33,4 +35,4 @@ bool hash_table_insert(word* w);
 word* hash_table_lookup(char* lookWord);
 
 #endif
-//*****************************************************************************
+//*****************************************************************************************
